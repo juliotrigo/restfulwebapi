3b600abb02de1968cfbcc9ff72982f25963e9cfd
juliotrigo <juliotrigocom@gmail.com>
2013-04-21 19:35:00 +0100

    Middleware improvement.

--- restfulwebapi/middleware.py
+++ restfulwebapi/middleware.py
@@ -18,20 +18,22 @@ class MIMETypeMiddleware(object):
         #    print('MIMETypeMiddleware initiated.')
     
     def process_request(self, request):
-        try:
-            #if settings.DEBUG:
-            #    print('Accept: %s' % request.META['HTTP_ACCEPT'])
-            #    print('Content-type: %s' % request.META['CONTENT_TYPE'])
-            #    print('MIMETypeMiddleware process_request. ' + request.path)
-            accept = request.META['HTTP_ACCEPT']
-            content_type = request.META['CONTENT_TYPE']
-        except KeyError:
-            #return HttpResponse(content='Unsupported Media Type', content_type='text/html', status=415)
-            return None
+        #try:
+        #    accept = request.META['HTTP_ACCEPT']
+        #    content_type = request.META['CONTENT_TYPE']
+        #    if settings.DEBUG:
+        #        print('Accept: %s' % accept)
+        #        print('Content-type: %s' % content_type)
+        #        print('MIMETypeMiddleware process_request. ' + request.path)
+        #except KeyError:
+        #    return HttpResponse(content='Unsupported Media Type', content_type='text/html', status=415)
         
         return None
     
     def process_response(self, request, response):
         #if settings.DEBUG:
         #    print('MIMETypeMiddleware process_response. ' + request.path)
+        #    print('Content-type: %s' % response['Content-Type'])
+        #Content-Type: application/prs.restfulwebapi+xml; charset=utf-8; version=1.0
+        #Vary: Content-Type
         return response
